78b7bf6e721ad04427dd6c7296fb20433377fe0d
wertoto <59776067+wertoto@users.noreply.github.com>
2021-04-05 01:31:43 -0500

    app.py start index.html

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from azure_get_unet import get_mask
 import requests
 import azure_get_atmosphere
 import firespread
+import io
 
 
 
@@ -35,6 +36,31 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+def save_img(image_obj):
+    ORIGINAL_IMAGE_DIR = 'static/images/uploads/original_img.png'
+    image_obj.save(ORIGINAL_IMAGE_DIR)
+    return ORIGINAL_IMAGE_DIR
+
+def save_mask(image_mask_np, shape):
+    MASK_IMAGE_DIR = 'static/images/uploads/mask.png'
+    mask = Image.fromarray(image_mask_np)
+    mask = mask.resize((shape[1], shape[0]))
+    print('mask reshaped to' + str(shape[0]) + 'x' + str(shape[1]))
+    mask.save(MASK_IMAGE_DIR)
+    return MASK_IMAGE_DIR
+
+def get_img_shape(img_dir):
+    img = Image.open(img_dir)
+    img = asarray(img)
+    return img.shape
+
+def get_prediction_frame(mask_obj_arr):
+    mask_obj_arr = np.array(mask_obj_arr)
+    ret, mask_encoded = cv2.imencode('.jpg', mask_obj_arr)
+    mask_encoded = mask_encoded.tobytes()
+    return (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + mask_encoded + b'\r\n')
+
 
 
 @app.route('/')
@@ -42,28 +68,39 @@ def home():
     print('hi')
     return render_template("index.html")
 
-
+@app.route('/show_prediction')
+def show_prediction(mask_obj_arr):
+    return Response(get_prediction_frame(mask_obj_arr), mimetype='image/png; boundary=frame')
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/original_image')
+def original_image():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.route('/another_page.html', methods=['GET', 'POST'])
 def another_page():
     print(request.method)
+    saved_dir =''
+    mask_dir = ''
     if request.method == 'GET':
         return render_template('another_page.html')
     if request.method == 'POST':
         if request.files:
             image = request.files['image']
-            image.save('uploads/images/test.png')
-            print(type(image), 'image saved')
+            saved_dir = save_img(image)
+            shape = get_img_shape(saved_dir)
+            print(shape)
+            # now we will also get the segmentation mask for the original image
+            mask = get_mask(saved_dir)
+            show_prediction(mask)
+            mask_dir = save_mask(mask, shape)
 
-    return render_template('another_page.html')
+    return render_template('another_page.html', original_image=saved_dir)
 
-@app.route('/show_prediction')
-def show_prediction():
-    return send_file('/uploads/images/lets-see.png', mimetype='image/png')
+
 
 
 if __name__ == '__main__':
